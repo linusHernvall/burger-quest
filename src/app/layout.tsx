a2a9d850c4311463504b1160fa2c891170d783be
linusHernvall <linus.hernvall@gmail.com>
2025-06-09 13:58:25 +0200

    Background image added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
+
 import Header from "@/components/header";
 
 import "./globals.css";
@@ -30,9 +31,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
-        <Toaster />
+        <div
+          className="min-h-screen bg-cover bg-center bg-no-repeat"
+          style={{ backgroundImage: "url('/paper.jpg')" }}
+        >
+          <Header />
+          {children}
+          <Toaster />
+        </div>
       </body>
     </html>
   );
