8a9e3486ab7582577d94b455b121c7133c2aef16
linusHernvall <linus.hernvall@gmail.com>
2025-08-05 13:37:21 +0200

    Changed interlude text and size.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,9 +36,8 @@ export default function Home() {
   return (
     <div className="mx-auto max-w-5xl py-10 px-4 md:py-20 xl:container">
       <h1 className="text-4xl pb-8 font-bold">Välkommen till Burger Quest!</h1>
-      <p className="pb-4">
-        Se en hamburgarjakt utvecklas när vi försöker hitta den bästa
-        hamburgaren.
+      <p className="pb-4 text-lg">
+        Tillsammans försöker vi hitta den bästa burgaren.
       </p>
       <AddBurgerButton />
       <ToastClientWrapper />
